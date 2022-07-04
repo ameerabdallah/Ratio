12a990de3989ef61295dd435ccbc031050900322
Ameer Abdallah <ameeryabdallah1@gmail.com>
2022-07-04 01:41:03 -0700

    added title and moved down reset button

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import "./index.css";
 import "./App.css";
 
 const lightGreen = "#4CAF50";
+const soundOn = false;
 
+const App = () => {
+  const yaySoundEffect = new Audio("./yay.mp3");
+  const naySoundEffect = new Audio("./nay.mp3");
+  const ehSoundEffect = new Audio("./eh.mp3");
 
-function App() {
   const posLS = "pos";
   const negLS = "neg";
   const neuLS = "neu";
+  const titleLS = "title";
   const [pos, setPos] = React.useState(
     parseInt(localStorage.getItem(posLS) || "0")
   );
@@ -19,11 +24,17 @@ function App() {
     parseInt(localStorage.getItem(neuLS) || "0")
   );
 
+  const [title, setTitle] = React.useState(
+    localStorage.getItem(titleLS) || ""
+  );
+
   React.useEffect(() => {
+    document.title = "Yay, Nay or Eh";
     localStorage.setItem(posLS, `${pos}`);
     localStorage.setItem(negLS, `${neg}`);
     localStorage.setItem(neuLS, `${neu}`);
-  }, [pos, neg, neu]);
+    localStorage.setItem(titleLS, `${title}`);
+  }, [pos, neg, neu, title]);
 
   const clamp = (x: number) => {
     return Math.max(x, 0);
@@ -38,7 +49,6 @@ function App() {
   }) => {
     const { a, b, aColor, bColor, ratioAsPercentage } = props;
     let ratio: any = a / b;
-
   
     let ratioColor: string;
     if (a > b) {
@@ -81,15 +91,19 @@ function App() {
     value: number;
     buttonClassName: string;
     onClick: (value: number) => void;
+    soundEffect: HTMLAudioElement;
   }) => {
-    const { value, buttonClassName, onClick } = props;
+    const { value, buttonClassName, onClick, soundEffect } = props;
     return (
       <div className="counter">
         <button className={`btn minus ${buttonClassName}`} onClick={() => onClick(clamp(value - 1))}>
           -
         </button>
         <span className={`value ${buttonClassName}`}>{value}</span>
-        <button className={`btn plus ${buttonClassName}`} onClick={() => onClick(clamp(value + 1))}>
+        <button className={`btn plus ${buttonClassName}`} onClick={() => {
+            onClick(clamp(value + 1));
+            !soundOn || soundEffect.play();
+          }}>
           +
         </button>
       </div>
@@ -98,35 +112,41 @@ function App() {
 
   return (
     <div id="page">
-      <button
-        className="reset"
-        onClick={() => {
-          setPos(0);
-          setNeg(0);
-          setNeu(0);
-        }}
-      >
-        RESET
-      </button>
+      <header>
+        <input type="text" value={title} placeholder="Title" spellCheck="false" onChange={(event) => setTitle(event.target.value)}/>
+      </header>
       <body>
         <div id="left" className="content">
-          <Counter value={pos} buttonClassName="pos" onClick={setPos} />
-          <Counter value={neg} buttonClassName="neg" onClick={setNeg} />
-          <Counter value={neu} buttonClassName="neu" onClick={setNeu}/>
+          <Counter value={pos} buttonClassName="pos" onClick={setPos} soundEffect={yaySoundEffect} />
+          <Counter value={neg} buttonClassName="neg" onClick={setNeg} soundEffect={naySoundEffect} />
+          <Counter value={neu} buttonClassName="neu" onClick={setNeu} soundEffect={ehSoundEffect} />
         </div>
         <div id="right" className="content">
           <div id="posRatios" className="ratios">
-            <h4>Positive</h4>
+            <h4>Yay</h4>
             <Ratio a={pos} b={neg} aColor={lightGreen} bColor={"red"}/>
             <Ratio a={pos} b={pos + neg + neu} aColor={lightGreen} ratioAsPercentage={true}/>
           </div>
           <div id="negRatios" className="ratios">
-            <h4>Negative</h4>
+            <h4>Nay</h4>
             <Ratio a={neg} b={pos} aColor={"red"} bColor={lightGreen} />
             <Ratio a={neg} b={neg + pos + neu} aColor={"red"} ratioAsPercentage={true}/>
           </div>
         </div>
       </body>
+      <button
+        className="reset"
+        onClick={() => {
+          // eslint-disable-next-line no-restricted-globals
+          if (confirm("Are you sure you want to reset?")) {
+            setPos(0);
+            setNeg(0);
+            setNeu(0);
+          }
+        }}
+      >
+        RESET
+      </button>
     </div>
   );
 }
